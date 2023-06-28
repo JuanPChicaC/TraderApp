03ab6995c1f8a458bfb0ccd05f9a9ee8a3306a26
Juan Pablo Chica <clasegit@exsis.com.co>
2023-06-28 13:39:41 -0500

    docs(app): the complete documentation for the API was setted

--- API/main.py
+++ API/main.py
@@ -2,13 +2,18 @@
 
 from fastapi import FastAPI
 from routes.supplier import external_api
+from routes.watchlist import watchlist_routes
 from tools.database import create_tables
-
-app = FastAPI()
-
-app.include_router(
-    external_api 
+from tools.common import get_api_doc
+app = FastAPI(
+    **get_api_doc()
     )
 
+app.include_router(
+    external_api,
+    )
+app.include_router(
+    watchlist_routes 
+    )
 create_tables()
 
